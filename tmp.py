71774e2a59ee2f87f7cef9ebea57a59919976baa
Артём <selemetartem@gmail.com>
2025-06-07 21:34:23 +0000

    Update index.html and add tmp.py for deploy

---
+++ tmp.py
@@ -0,0 +1,7 @@
+# tmp.py
+file_path = "/workspaces/Tic-tac-toe_with_optimal_AI_telegram_bot/tictactoe-bot-website/index.html"
+
+with open(file_path, "r", encoding="utf-8") as f:
+    content = f.read()
+    print(content[:500])  # или обрабатывай как нужно
+
